7b6ff1ac8ef353f9fbcee33fd4fe6733629d3e8f
ElKoax <47944778+ElKoax@users.noreply.github.com>
2019-05-12 11:23:42 +0100

    Add files via upload

---
+++ V3rmAPI-Example/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using V3rmAPI;
+
+namespace V3rmAPI_Example
+{
+    public partial class Form1 : Form
+    {
+        V3rmAPI.V3rmAPI API_Example = new V3rmAPI.V3rmAPI();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Grabs the API
+            API_Example.ExecuteScript(richTextBox1.Text);
+            // Then it Executes any text that is inputted into the textbot.
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // This Ininilizes the Injection.
+            API_Example.Inject();
+        }
+    }
+}
